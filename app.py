3e388c9e62d53be63a1a8a44bfa206b43cf6002d
aizikgregory12 <aizik.gregory@gmail.com>
2024-08-02 09:54:49 -0400

    Flask code

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
+import pyqrcode
+import io
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/generate_qr', methods=['POST'])
+def generate_qr():
+    data = request.json
+    url = data.get('url')
+    title = data.get('title')
+
+    qr = pyqrcode.create(url)
+    buffer = io.BytesIO()
+    qr.png(buffer, scale=6)
+    buffer.seek(0)
+
+    return send_file(buffer, mimetype='image/png', as_attachment=True, download_name='qr_code.png')
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
